7f57945eb3b136a888c01cb223c94b5a88a1a2b9
NekrozX4 <dimbiniaina.fitahiana@gmail.com>
2023-12-24 01:23:10 +0300

    bg mapme

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
@@ -7,32 +7,40 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [showErrorPopup, setShowErrorPopup] = useState(false);
-  const [showInputError, setShowInputError] = useState(false); 
+  const [showSuccessPopup, setShowSuccessPopup] = useState(false)
+  const [showInputError, setShowInputError] = useState(false);
   const navigate = useNavigate('');
 
   const handleLogin = async () => {
     try {
-      if (!email || !password) {
-        setShowInputError(true);
-        return;
-      }
-  
-      setShowInputError(false);
-
       const usersResponse = await fetch('http://localhost:8081/utilisateur');
       const usersData = await usersResponse.json();
   
-      const user = usersData.find((user) => user.Us_login === email && user.Us_pwd === password);
+      const user = usersData.find(
+        (user) => user.Us_login === email && user.Us_pwd === password
+      );
   
       if (user) {
         if (user.Fo_id === 1) {
           localStorage.setItem('loggedInUser', JSON.stringify(user));
           console.log('Logged-in user data:', user);
           console.log('Login successful! Redirecting to user profile');
-          navigate(`/main/${user.Us_matricule}`, { state: { loggedInUser: user } });
+          setShowSuccessPopup(true);
+  
+          // Delay the navigation after showing the success popup
+          setTimeout(() => {
+            navigate(`/main/${user.Us_matricule}`, {
+              state: { loggedInUser: user },
+            });
+          }, 3000); // Adjust the delay as needed
         } else if (user.Fo_id === 2) {
           console.log('User does not have admin privileges.');
-          navigate('/saisie');
+          setShowSuccessPopup(true);
+  
+          // Delay the navigation after showing the success popup
+          setTimeout(() => {
+            navigate('/saisie');
+          }, 3000); // Adjust the delay as needed
         }
       } else {
         console.log('Invalid credentials.');
@@ -43,20 +51,32 @@ const Login = () => {
       setShowErrorPopup(true);
     }
   };
+  
+
+  useEffect(() => {
+    // Close the error popup after 3000 milliseconds (adjust as needed)
+    const timeoutId = setTimeout(() => {
+      setShowErrorPopup(false);
+      setShowSuccessPopup(false); // Corrected from showSuccessPopup to setShowSuccessPopup
+    }, 3000);
+  
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [showErrorPopup, showSuccessPopup]);
+
+  
+  
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const closeErrorPopup = () => {
-    setShowErrorPopup(false);
-  };
-
   return (
     <div className='main-container'>
-      <div className='login-container'> 
+      <div className='login-container'>
         <div className='login-section'>
-        <div className='about-image'></div>
+          <div className='about-image'></div>
           <h2>LOGIN</h2>
           <input
             placeholder={`Login${showInputError ? ' - veuillez remplir ce champ' : ''}`}
@@ -65,13 +85,15 @@ const Login = () => {
             onChange={(e) => setEmail(e.target.value)}
             className={showInputError ? 'input-error' : ''}
           />
-          <br /> 
+          <br />
           <input
-            placeholder={`Password${showInputError ? ' - veuillez remplir ce champ' : ''}`}
+            placeholder={`Password${
+              showInputError ? ' - veuillez remplir ce champ' : ''
+            }`}
             type={showPassword ? 'text' : 'password'}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className={showInputError ? 'input-error' : ''} 
+            className={showInputError ? 'input-error' : ''}
           />
           <br />
           <p className='hide-show' type='button' onClick={togglePasswordVisibility}>
@@ -84,13 +106,21 @@ const Login = () => {
         </div>
       </div>
       {showErrorPopup && (
-        <div className="error-popup" onClick={closeErrorPopup}>
-          <div className="popup-content">
+        <div className='error'>
+          <div className='popup'>
             <p>Login failed. Please check your credentials or user privileges.</p>
-            <button onClick={closeErrorPopup}>Close</button>
           </div>
         </div>
       )}
+      {
+        showSuccessPopup && (
+          <div className='success-login'>
+          <div className='success-login-content'>
+            <p>Login successful</p>
+          </div>
+        </div>
+        )
+      }
     </div>
   );
 };
